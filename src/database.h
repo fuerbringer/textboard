db07c43ea5b9e1a79aa11efe64179feb28a73b6a
shwsh <thatweirdbear@tuta.io>
2018-02-17 08:30:05 +0700

    Add post deletion function

--- src/database.h
+++ src/database.h
@@ -7,5 +7,5 @@ struct db_thread_params {
     pthread_rwlock_t dblock;
 };
 
-void db_thread_save(FILE **db_file_ptr, struct post_list **curr_post_list_ptr, int save_headers);
+void db_thread_save(FILE **db_file_ptr, struct post_list **curr_post_list_ptr);
 void *db_thread_main(void *db_thread_params_ptr);
--- src/posts.h
+++ src/posts.h
@@ -15,8 +15,9 @@ struct post_list *post_list_create();
 void post_list_destroy(struct post_list *list);
 void post_list_prepend(struct post_list *list, struct post *post);
 void post_list_append(struct post_list *list, struct post *post);
-char *post_list_render(struct post_list *list, const int is_reply);
-struct post *post_list_find(struct post_list *list, int id);
+char *post_list_render(struct post_list *list, int is_reply);
+struct post *post_list_find(struct post_list *list, const unsigned int id);
+struct post *post_list_findr(struct post_list *list, const unsigned int id);
 void post_list_bump(struct post_list *list, struct post *post);
 void post_list_debug(struct post_list *list);
 
@@ -27,15 +28,21 @@ struct post {
     char *author;
     char *subject;
     char *comment;
+    char *delete_passwd;
     time_t created_time;
-    int saved;
+    int saved, deleted;
     struct post_list *replies;
     struct post *prev, *next;
     struct post *parent; // don't free me
 };
 unsigned int global_id;
 
-struct post *post_create(unsigned int id, const char *author, const char *subject, const char *comment, time_t created_time, struct post *parent);
+struct post *post_create(
+	const unsigned int id, const char *author, const char *subject, const char *comment,
+	const char* delete_passwd, time_t created_time, struct post *parent
+);
+void post_destroy_no_list(struct post *post);
 void post_destroy(struct post *post);
 char *post_render(struct post *post);
+void post_delete(struct post *post);
 void post_debug(struct post *post);
